6db1a6dde7d6678bde726ef6537cf603dea8e6d9
ravisu05 <160301581+ravisu05@users.noreply.github.com>
2024-03-16 15:37:06 +0530

    YU

--- lib/regscreen.dart
+++ lib/regscreen.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+import 'loginscreen.dart';
+
 
 class regiscrn extends StatefulWidget {
   const regiscrn({Key? key}) : super(key: key);
@@ -10,6 +16,42 @@ class regiscrn extends StatefulWidget {
 }
 
 class _regiscrnState extends State<regiscrn> {
+
+
+  final _rolNoController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool loading=false;
+
+  Future<void> regiscrn() async {
+    Uri uri = Uri.parse('url');
+    var response = await http.post(uri,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: jsonEncode({
+          'name': _nameController.text,
+          'email': _emailController.text,
+          'rollno': _rolNoController.text,
+          'password': _passwordController.text
+        }));
+    var data = jsonDecode(response.body);
+
+    print(data['message']);
+    if (response.statusCode == 200) {
+      Navigator.push(context, MaterialPageRoute(
+          builder: (context) => const LoginScreen ()),
+      );
+    }
+    else {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Registation field')));
+
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,6 +67,7 @@ class _regiscrnState extends State<regiscrn> {
               Container(
                   width: 600,
                   child: TextField(
+                    controller: _nameController,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
                       hintText: 'Enter your name',
@@ -35,6 +78,7 @@ class _regiscrnState extends State<regiscrn> {
               Container(
                 width: 600,
                 child: TextField(
+                  controller: _rolNoController,
                     decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: 'Enter your roll no',
@@ -45,6 +89,7 @@ class _regiscrnState extends State<regiscrn> {
               Container(
                 width: 600,
                 child: TextField(
+                  controller: _emailController,
                     decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: 'Enter your email',
@@ -55,6 +100,7 @@ class _regiscrnState extends State<regiscrn> {
               Container(
                 width: 600,
                 child: TextField(
+                  controller: _passwordController,
                     decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: 'Enter your password',
@@ -63,7 +109,9 @@ class _regiscrnState extends State<regiscrn> {
               ),
               SizedBox(height: 30),
               TextButton(
-                onPressed: () {},
+                onPressed: () {
+                  regiscrn();
+                },
                 style: TextButton.styleFrom(backgroundColor: Colors.white60),
                 child: Text(
                   'Register',
